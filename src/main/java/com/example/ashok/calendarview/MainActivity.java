36c56f206555ed19ba48bba5c06a23da7c96accd
Ashok536 <ashokreddy565ar@gmail.com>
2017-01-28 23:43:02 +0530

    Add files via upload

---
+++ src/main/java/com/example/ashok/calendarview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.ashok.calendarview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+    CalendarView calendarView;
+    TextView dateDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        dateDisplay = (TextView) findViewById(R.id.date_display);
+        dateDisplay.setText("Date: ");
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener()
+        {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2)
+            {
+                dateDisplay.setText("Date: " + i2 + " / " + i1 + " / " + i);
+
+                Toast.makeText(getApplicationContext(), "Selected Date:\n" + "Day = " + i2 + "\n" + "Month = " + i1 + "\n" + "Year = " + i, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
